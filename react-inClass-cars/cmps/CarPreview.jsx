13e68473e39280a34ad765b9cfc98ccb992dfc72
yinonab <yinonabarjel@gmail.com>
2023-09-04 00:31:36 +0300

    fix git

--- react-inClass-cars/cmps/CarPreview.jsx
+++
@@ -1,9 +0,0 @@
-export function CarPreview({ car }) {
-    return (
-        <article className="car-preview">
-            <h2>Car Vendor: {car.vendor}</h2>
-            <h4>Car Speed: {car.maxSpeed}</h4>
-            <img src={`../assets/img/${car.vendor}.png`} alt="" />
-        </article>
-    )
-}
--- react-inClass-cars/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export function Home() {
-    
-    return (
-        <section className="home">
-            <h1>Car's R Us!</h1>
-            <img src="../assets/img/react.png" alt="" />
-        </section>
-    )
-}
